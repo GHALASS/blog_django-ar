417f5b05ddbb3e38f95af435415812ac0c432925
ghalass <msghalas@gmail.com>
2021-01-05 20:48:16 +0000

    Display Posts

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'title':'التدوينة الاولى',
+        'content':' نص التدوينة الاولى',
+        'post_date':'15-3-2019',
+        'author':'Ahmed Abouissa'
+    },
+    {
+        'title':'التدوينة الثانية',
+        'content':' نص التدوينة الثانية',
+        'post_date':'15-3-2019',
+        'author':'Ahmed Abouissa'
+    }, 
+    {
+        'title':'التدوينة الثالثة',
+        'content':' نص التدوينة الثالثة',
+        'post_date':'15-3-2019',
+        'author':'Ahmed Abouissa'
+    }, 
+    {
+        'title':'التدوينة الرابعة',
+        'content':' نص التدوينة الرابعة',
+        'post_date':'15-3-2019',
+        'author':'Ahmed Abouissa'
+    }, 
+    {
+        'title':'التدوينة الخامسة',
+        'content':' نص التدوينة الخامسة',
+        'post_date':'15-3-2019',
+        'author':'Ahmed Abouissa'
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html', {'title':'Home'})
+    context = {
+        'title':'الرئيسية',
+        'posts': posts
+    }
+    return render(request, 'blog/index.html', context)
