f8d896aa160c3d8902895fb68b7011a64493bd76
ghalass <msghalas@gmail.com>
2021-01-06 14:46:25 +0000

    Add form login

--- src/user/forms.py
+++ src/user/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserCreationForm(forms.ModelForm):
-    username = forms.CharField(label='إسم المستخدم', max_length=30)
-    email = forms.EmailField(label='البريد الإلكتروني', max_length=30)
-    first_name = forms.CharField(label='الإسم الاول', max_length=30)
-    last_name = forms.CharField(label='الإسم الثاني', max_length=30)
+    username = forms.CharField(label='إسم المستخدم', max_length=30, help_text='إسم المستخدم يجب أن لا يحتوي على مسافات.')
+    email = forms.EmailField(label='البريد الإلكتروني')
+    first_name = forms.CharField(label='الإسم الاول')
+    last_name = forms.CharField(label='الإسم الثاني')
     password1 = forms.CharField(label='كلمة المرور', widget=forms.PasswordInput, min_length=8)
     password2 = forms.CharField(label='تاكيد كلمة المرور', widget=forms.PasswordInput, min_length=8)
     class Meta:
@@ -14,7 +14,7 @@ class UserCreationForm(forms.ModelForm):
             'last_name', 'password1', 'password2')
     def clean_password2(self):
         cd = self.cleaned_data
-        if cd['password1'] != cd['passwprd2']:
+        if cd['password1'] != cd['password2']:
             raise forms.ValidationError('كلمة المرور غير متطابقة')
         return cd['password2']
     
--- src/user/views.py
+++ src/user/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            # messages.success(request, 'تهانينا  {} لقد تم التسجيل بنجاح'.format(username))
+            messages.success(request, f'تهانينا  {username} لقد تم التسجيل بنجاح')
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'user/register.html', {
         'title':'التسجيل',
         'form':form,
